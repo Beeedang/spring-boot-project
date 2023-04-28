ebdd2b2e53cfc945327d338c90d5a812caaf116e
jinwoo <jinwooh@jinwooui-MacBookAir.local>
2023-04-28 10:43:04 +0900

    test

---
+++ src/test/java/com/playdata/springbootproject/web/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.playdata.springbootproject.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())  // isok 메소드는 http 200인지 확인
+                .andExpect(content().string("hello"));
+    }
+}
